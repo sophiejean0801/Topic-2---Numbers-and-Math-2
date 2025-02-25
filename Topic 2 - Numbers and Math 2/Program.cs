e04bf0b1f4f99d294d3145fe0e7b34e28ea7d455
Sophie Jean <363449299@GoTVDSB.ca>
2025-02-25 13:58:51 -0500

    Add project files.

---
+++ Topic 2 - Numbers and Math 2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_2___Numbers_and_Math_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Topic 2 - Numbers and Math Assignment 
+            // Sophie Jean
+            Console.WriteLine("I'm" + (2025 - 2008) + "years old");
+            Console.WriteLine();
+            Console.WriteLine("The answer is" + (14 + 15 + 16) / 3);
+            Console.WriteLine();
+            Console.WriteLine("The answer is" + (14.0 + 15.0 + 16.0 + 17.0 / 4));
+            Console.WriteLine();
+            Console.WriteLine("I'm" + (2025 - 2008) + "years, 10 months and 2 days old");
+            Console.WriteLine();
+            Console.WriteLine("The answer is" + (14 + 15 + 16) / 3);
+            Console.WriteLine();
+            Console.WriteLine("The answer is" + (14.0 + 15.0 + 16.0 + 17.0 / 4));
+            Console.WriteLine();
+            Console.WriteLine("The area is" + 2 * (3 * 3));
+            Console.WriteLine();
+            const double pi = 3.14159; // Math.PI is a constant in the Math class
+            Console.WriteLine("The area is" + pi * (3 * 3));
+            Console.WriteLine();
+            Console.WriteLine("The program knows when to add things when both terms are numbers and they are in between brackets");
+            Console.ReadLine();
+        }
+    }        
+}
